9ad7afa4006a4a2d2b7113c7c8d53188a6484f83
mmgbeojiri <150837937+mmgbeojiri@users.noreply.github.com>
2025-09-11 13:30:22 +0000

    recompile

--- Lesson-3b/Main.java
+++ Lesson-3b/Main.java
@@ -1,5 +1,8 @@
 class Dox {
     public static void main(String[] args) {
+        
         System.out.println("Michael Scott \n85-63 212 St"); // fake information i can't do ts
+        // javac ./Main.java
+        // java Dox
     }
 }
