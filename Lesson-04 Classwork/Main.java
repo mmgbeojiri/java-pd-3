584c1c77ba882650adcd52e9099724b0569fbf73
mmgbeojiri <150837937+mmgbeojiri@users.noreply.github.com>
2025-09-15 13:29:30 +0000

    lesson 4

---
+++ Lesson-04 Classwork/Main.java
@@ -0,0 +1,70 @@
+class Main {
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+ void init(){
+/*  
+    Challenge 1:
+    Create a variable to store and display its value (assign a value to each variable):
+    1) student's age 
+    2) number of people that live with you
+    3) number of items in a cart 
+*/
+  
+ int age = 25;
+ int peopleWhoLiveWithMe = 25;
+ int numOfItems = 25;
+ 
+
+
+/*  
+    Challenge 2:
+    Create a variable to store and display its value (assign a value to each variable):
+    1) temperature 
+    2) student's GPA
+    3) growth rate of your investment
+*/
+
+
+double temperature = 79.4;
+double GPA = 0.12;
+double growthRate = 2.71;
+
+/*  
+    Challenge 3:
+    Create a variable to store and display its value (assign a value to each variable):
+    1) Students last name 
+    2) students first initial
+    3) description of an item 
+*/
+
+String lname = "Smith";
+char initial = 'J';
+String desc = "only 4 monthly payments of $149.99";
+
+
+
+/*  
+    Challenge 4:
+    Create a variable to store and display its value (assign a value to each variable):
+    1) the status whether lights are on
+    2) the status whether the game is over
+    3) the status whether the battery is charging
+*/
+
+boolean lightsOn = true;
+boolean gameOver = true;
+boolean charging = true;
+
+
+
+
+
+    // **************************************************
+    // **** Don't write any code below here.  ***********
+    // **************************************************
+    
+    //nah
+  }
+}
