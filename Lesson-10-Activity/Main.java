fb886754b2547c18a1ae8e7fd1c282382f1f7880
mmgbeojiri <150837937+mmgbeojiri@users.noreply.github.com>
2025-10-09 12:54:35 +0000

    did yo functoiuns

---
+++ Lesson-10-Activity/Main.java
@@ -0,0 +1,55 @@
+
+class Main {
+
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+	double gpa(double inputGPA) {
+		if (inputGPA > 90) {
+			return inputGPA *1.1;
+		} else {
+			return inputGPA;
+		}
+	}
+
+	boolean isGraduating(int grade, int credits) {
+		if (grade >= 12 && credits >= 44) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	String BMI(double weight, double inches) {
+		double bmi = (weight / Math.pow(inches, 2)) * 703;
+		if (bmi < 18.5) {
+			return "Underweight";
+		} else if (bmi >= 18.5 && bmi <= 24.9) {
+			return "Normal";
+		} else if (bmi >= 25 && bmi <= 39.9) {
+			return "Overweight";
+		} else if (bmi >= 40) {
+			return "Obese";
+		}
+		// default
+		return "Enter valid weight and height";
+	}
+
+	boolean blueOrViolet(double tHz) {
+		if (tHz >= 600 && tHz <= 670) {
+			return true;
+		} else if (tHz >= 700 && tHz >= 750) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	void init(){
+		
+   
+
+  }
+
+ 
+  
+}
