84de25dc604e64a5bc3ac9de293e2e8e95a74123
mmgbeojiri <150837937+mmgbeojiri@users.noreply.github.com>
2026-03-16 13:30:53 +0000

    lesson 42 b

--- Lesson-42-Activity/Main.java
+++ Lesson-42-Activity/Main.java
@@ -23,9 +23,19 @@ class Main {
 
 		Database db = new Database("jdbc:sqlite:students.db" );
 
-    String sql1 = "SELECT * FROM CR101 WHERE StudentID = 'STUDENT1'";
+    String sql1 = "UPDATE CR101 SET Room = 122 WHERE Period = 3 AND StudentID = 'STUDENT1001'";
+    String sql2 = "DELETE FROM CR101 WHERE StudentID = 'STUDENT1200' AND Course = 'ZQCTEDA'";
+    String sql3 = "UPDATE CR101 SET Room = 213 WHERE Teacher1 = 'DOYLE' AND Period IN (4,5)";
+    String sql4 = "UPDATE CR101 SET Teacher1 = 'Mr ROFFLER' WHERE Teacher1 = 'MR ARCHETTI' AND Course = 'MQF44QGF'";
+    String sql5 = "UPDATE CR101 SET Grade = 11, OffClass = 'Junior' WHERE StudentID = 'STUDENT999'";
+    String sql6 = "INSERT INTO CR101 ('Course', 'Section', 'Teacher1', 'Period, 'Room', 'StudentID') VALUES ('MKUFTC6',1,'CASTRO R',9,322,'student1231')";
 
-    print(db.runSQL(sql1, "table-auto");)	
+    db.runSQL(sql1, "table-auto");	
+    db.runSQL(sql2, "table-auto");	
+    db.runSQL(sql3, "table-auto");	
+    db.runSQL(sql4, "table-auto");
+    db.runSQL(sql5, "table-auto");	
+    db.runSQL(sql6, "table-auto");
     
    
      
